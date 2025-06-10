7cd7c920d77068c8282c720f9ace0a2f9573ea75
Sidharth <sksidhu142@gamil.com>
2025-06-10 18:08:56 +0200

    comments removed from the ECR

--- ECR/ecr_policies.tf
+++ ECR/ecr_policies.tf
@@ -8,11 +8,6 @@ data "aws_iam_policy_document" "registry_policy" {
       identifiers = ["arn:aws:iam::222222222222:role/admin"] # Replace with your actual role ARN
     }
 
-    # actions = [
-    #   "ecr:GetDownloadUrlForLayer",
-    #   "ecr:BatchGetImage",
-    #   "ecr:BatchCheckLayerAvailability"
-    # ]
     actions = ["ecr:*"]
 
     resources = ["*"]
